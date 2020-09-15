7256d8717654cb0f97d1d88b40263a77c3e6ca62
kanstantsin_karpau <Kanstantsin_Karpau@epam.com>
2020-09-16 02:14:01 +0300

    fix ip zabbix server for client; fix tomcat service enable-restart

--- zabbix/lab_1/run_client.sh
+++ zabbix/lab_1/run_client.sh
@@ -7,7 +7,7 @@ cat << SCRIPT | sudo tee /etc/zabbix/zabbix_agentd.conf
 PidFile=/var/run/zabbix/zabbix_agentd.pid
 LogFile=/var/log/zabbix/zabbix_agentd.log
 LogFileSize=0
-Server=$server_adress
+Server=${server_adress}
 ServerActive=${server_adress}
 Hostname=$HOSTNAME
 HostMetadataItem=system.uname
@@ -21,5 +21,5 @@ sudo yum install -y tomcat tomcat-webapps tomcat-admin-webapps
 sudo sed -i '/<\/tomcat-users>/i <user name="admin" password="12345678" roles="admin,manager,admin-gui,manager-gui,manager-status" \/>' /etc/tomcat/tomcat-users.xml
 
 sudo chmod -R 775 /var/log/tomcat
-sudo systemctl enable logstash
-sudo systemctl restart logstash
+sudo systemctl enable tomcat
+sudo systemctl restart tomcat
